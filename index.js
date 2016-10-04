a2f6ba0e7496a3699b73ef5425dc9a56ed79b49d
TosakaZ <amenbows@yahoo.co.jp>
2016-10-04 17:28:10 +0000

    サーバーで HTML を表示するように改良

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain',
+    'Content-Type': 'text/html',
     'charset': 'utf-8'
   });
-  res.write(req.headers['user-agent']);
+  const head = '<head><meta charset="UTF-8"><title>HTTP サーバー - 練習問題 ｜ N予備校</title></head>';
+  const body = '<body><h1>HTMLの一番大きい見出しを表示します</h1></body>';
+  const html = '<!DOCTYPE html><html lang="ja">' + head + body + '</html>';
+  res.write(html);
   res.end();
 });
 const port = 8000;
 server.listen(port, () => {
   console.log('Listening on ' + port);
 });
+
