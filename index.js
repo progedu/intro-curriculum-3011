4474a8f5eee502388371df713a2362daf8899f0f
kimidera <fqrks223@i.softbank.jp>
2016-11-26 02:43:09 +0000

    HTTPサーバー改良

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain',
+    'Content-Type': 'text/html',
     'charset': 'utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1><h2>サーバーいじるの楽しい</h2><h3>鯖鰯鰰</h3></body></html>');
   res.end();
 });
 const port = 8000;
