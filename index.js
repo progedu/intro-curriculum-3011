d7863d837db92ee46d5638ab137cb1368e94b63f
200lux <tithrx@gmail.com>
2017-03-04 14:08:49 +0000

    アクセス時に h1 見出しを表示させるよう変更しました

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain',
-    'charset': 'utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
+    // 解答と同じ charset の書き方だと日本語が文字化けしてしまいましたが、
+    // 上の書き方で改善しました。
   });
   res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
   res.end();
 });
 const port = 8000;
