44646d4124f4a4bede623f8a1c79f63681f473a7
sshmdn <sshmdn.yahoo.co.jp>
2017-07-31 09:25:49 +0000

    UTF-8に対応

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const server = http.createServer((req, res) => {
     'Content-Type': 'text/html',
     'charset' : 'utf-8'
   });
-  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
+  res.write('<!DOCTYPE html><html lang="ja"' +
+    '<head><meta charset="UTF-8"></head>' +
+    '<body><h1>HTMLの一番大きい見出しを表示します</h1></body>' +
+    '</html>');
   res.end();
 });
 const port = 8000;
