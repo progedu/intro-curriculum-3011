967b4cda69b5cfc1126b5a9a2d06d5195d09a0df
davideryu2017 <davideryu2017@gmail.com>
2017-09-25 11:24:52 +0000

    HTML を返すように実装

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<script>document.write("<h3>にこにこ</h3>");</script>');
   res.end();
 });
 const port = 8000;
