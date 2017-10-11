1ce6c518d048f68441f0ee5b4636e1ea5a620096
gadget-tf <gadget.tf@gmail.com>
2017-10-11 13:20:46 +0000

    練習問題の解答の実装

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
+
+const html_header = {
+  'Content-Type': 'text/html; charset=utf-8'
+};
+const html_str = '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>';
+
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
+  res.writeHead(200, html_header);
+  res.write(html_str);
   res.end();
 });
 const port = 8000;
