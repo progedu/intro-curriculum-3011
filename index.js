eb63b0f961ef7af6c61a47a2b821f934955fe255
HiroshiSuka <himajin0164@gmail.com>
2017-10-28 10:52:44 +0000

    HTMLに変更

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+    res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.write('<!DOCTYPE html>');
+    res.write('<html lang="ja">');
+    res.write('<body>');
+    res.write('<h1>HTMLの一番大きい見出しを表示します</h1>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
 });
 const port = 8000;
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+    console.log('Listening on ' + port);
 });
