1612765f4d34b39d6e66866daa16170b364cee0f
daitokuful <daitokuful@gmail.com>
2017-11-28 10:00:27 +0000

    HTML を表示させる関数に変更

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+  fs.readFile('./read.html', 'utf-8', function(err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.write(data);
+    res.end();
   });
-  res.write(req.headers['user-agent']);
-  res.end();
 });
 const port = 8000;
 server.listen(port, () => {
