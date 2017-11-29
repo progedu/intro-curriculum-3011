fb99d0452f2d2eee0446c1b541b7cfedb501bfa0
Masashi Yoshitsugu <masashi_yosh@yahoo.co.jp>
2017-11-29 00:20:23 +0000

    html表示を実装

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
+  
+  //ファイルを読み込んで、callback関数を実行
+  fs.readFile('./test.html', 'utf-8', doReard);
+
+  //コンテンツを実行
+  function doReard(err, data) {
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  res.write(data);
   res.end();
+  }
 });
 const port = 8000;
 server.listen(port, () => {
