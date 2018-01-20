002bebe75f8395d36e7449c64c2aff36f9aed375
Kazunori Oyama <kazunori0326.t@gmail.com>
2018-01-20 13:42:11 +0000

    HTMLを返すように変更

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
+const response = '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  //res.write(req.headers['user-agent']);
+  res.write(response);
   res.end();
 });
 const port = 8000;
