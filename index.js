15655243b5180adf06aaffaee0eb460f51ded3f8
Yoshiki Nakahara <szk24nakahara@yahoo.co.jp>
2018-03-21 23:21:34 +0000

    できませんでした。

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 const http = require('http');
+const ht = <!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(req.headers['ht']);
   res.end();
 });
 const port = 8000;
