bdfa1e8ba6002a3f1e69cdc685ac7f45c972ebf5
Yousuke Nakano <yousuke.computer@gmail.com>
2018-07-27 11:00:44 +0000

    HTML を表示するように変更

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
+
+
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang = "ja"><body><h1>HTML の 1 番大きな見出しを表示しています</h1></body></html>')
   res.end();
 });
 const port = 8000;
