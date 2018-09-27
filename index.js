974909cee7223f64ce1b3d88caec83850f89c6ea
miifriends <miikun114514@gmail.com>
2018-09-27 11:14:24 +0000

    絵文字を追加

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTML の一番大きい見出しを表示します。</h1></body></html>');
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTML の一番大きい見出しを表示します。😻</h1></body></html>');
   res.end();
 });
 const port = 8000;
