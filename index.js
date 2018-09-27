b7ee9c1913d168d8bf36141bbd8b7ac0b4fb6d61
tobihaze <tobihaze_2017@yahoo.co.jp>
2018-09-27 11:07:46 +0000

    ユーザーエージェントを表示するように追記

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const server = http.createServer((req, res) => {
     'Content-Type': 'text/html; charset=utf-8'
   });
   res.write(
-    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
+    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1><h3>ユーザーエージェントはこちら↓</h3></body></html>'
   );
+  res.write(req.headers['user-agent']);
   res.end();
 });
 const port = 8000;
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+  console.log(`Listening on ${port}`);
 });
