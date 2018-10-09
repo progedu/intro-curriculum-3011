b98f30079ded8b56a3d1b4ad2253455f64c420e8
hilolih <hilolih@gmail.com>
2018-10-09 15:49:41 +0900

    hhtpサーバー：練習問題

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  //res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>')
   res.end();
 });
-const port = 8000;
+const port = 8080;
 server.listen(port, () => {
   console.log('Listening on ' + port);
 });
