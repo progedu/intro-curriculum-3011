146a385b9a1604c0c373ddf8efff40123870df8b
oriyoshi <yorishio.yoshio@gmail.com>
2018-10-19 03:03:06 +0000

    HTMLを表示し、ユーザーエージェントも表示するように変更

--- index.js
+++ index.js
@@ -2,9 +2,21 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  
+  //ユーザーエージェント情報を保存
+  let user_agent = req.headers['user-agent'];
+  //html文書作成
+  let html =
+  `<!DOCTYPE html>
+     <html lang="ja">
+       <body>
+         <h1>あなたのブラウザ情報を表示します</h1>
+         <h2>${user_agent}</h2>
+       </body>
+     </html>`;
+  res.write(html);
   res.end();
 });
 const port = 8000;
