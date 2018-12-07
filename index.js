e835a6bd846f840c8c0ea4bff82ab5e3e4fd5206
mnishida <peco.igo@gmail.com>
2018-12-07 16:07:23 +0000

    HTMLを表示するように変更

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const server = http.createServer((req, res) => {
     'Content-Type': 'text/html; charset=utf-8'
   });
   res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示しています</h1></body></html>');
-//  res.write('&lt;!DOCTYPE html&gt;&lt;html lang="ja"&gt;&lt;body&gt;&lt;h1&gt;HTMLの一番大きい見出しを表示しています&lt;/h1&gt;&lt;/body&gt;&lt;/html&gt;');
   res.end();
 });
 const port = 8000;
