b418b93e5f133e0d46bfb76890f234d30f591dca
gekka <gekkanomeado@yahoo.co.jp>
2018-12-20 07:43:49 +0000

    HTTPサーバーがHTMLを返すように実装

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します🍺🍝</h1></body></html>');
   res.end();
 });
 const port = 8000;
