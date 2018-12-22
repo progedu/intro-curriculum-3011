e959650bfbf69ec94fbb864cb4c0c56f0d7b3324
TANAKA Toru <tor.t2.tanaka@gmail.com>
2018-12-22 06:33:58 +0000

    表示変更

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 const http = require('http');
+const html_string='<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>';
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(html_string);
   res.end();
 });
 const port = 8000;
