7bdda01bd6c690ddb7b42240a68c1167b34829ae
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-03-04 11:30:19 +0000

    HTML表示

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 const http = require('http');
+// もしサーバ起動できなかったら、ps aux | grep nodeで既存プロセスを探して強制停止させること。
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
   res.end();
 });
 const port = 8000;
