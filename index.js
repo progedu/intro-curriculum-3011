3e5a0a7e99790be74089c6f92e1b98e7504d1dca
sh00049m <sh00049m@gmail.com>
2019-06-07 15:54:08 +0000

    Showing HTML Format instead of user-agent

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(fs.readFileSync('./index.html'));
   res.end();
 });
 const port = 8000;
