723dc285278f62f76cec7be88c2c21d4cf42dcc9
Hatoremi <hatoremi@gmail.com>
2019-09-27 20:15:22 +0900

    HTMLを表示するように修正

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><title>http-server</title><body><h1>Hello World !</h1><h2>HTMLの二番目に大きい見出しを表示します</h2></body></html>');
   res.end();
 });
 const port = 8000;
