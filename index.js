04e24028b63f4ccd725d5655df216d935a33e25d
piyo2019 <hakobe2001@yahoo.co.jp>
2019-09-30 18:12:05 +0900

    HTMLを表示させてみました

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(`
+  <!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1>
+  <h5>小さい見出しも表示してみます</h5>
+  バッククオートを使うと改行があってもOK (^^)　</body></html>
+  `);
   res.end();
 });
 const port = 8000;
