b7654fa84cbeec43faa39ac415150d017721d73f
yufu <atashism@gmail.com>
2020-03-17 09:28:20 +0000

    レスポンス内容を変更

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 const http = require('http');
+
+let body = '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>';
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(body);
   res.end();
 });
 const port = 8000;
