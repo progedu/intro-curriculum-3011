9f957a2522721f7898304b2952237b8ce0dc6a30
RyuPC <vbkit@outlook.com>
2020-03-25 18:32:43 +0900

    練習問題にHTMLファイルを追加

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 const http = require('http');
+const fs= require('fs');
+const htmlData = fs.readFileSync('./index.html');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'
+    
   });
-  res.write(req.headers['user-agent']);
+   res.write(htmlData);
   res.end();
 });
-const port = 8000;
+const port = 8002;
 server.listen(port, () => {
   console.log('Listening on ' + port);
 });
