a85427856c42f6be24ec81127bb1a55633542c71
chip-cocoa2020 <arkbuchessener1209@outlook.jp>
2020-03-31 19:14:50 +0900

    index.htmlを別ファイルとしてindex.jsに読み込み、htmlを表示

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+
+const htmlData = fs.readFileSync('./index.html');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(
-    `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>初めてのサーバー</title>
-    </head>
-    <body>
-        <h1>Node.js でサーバーを立てて、htmlを表示しました。</h1>
-    </body>
-    </html>`);
+  res.write(htmlData);  
   res.end();
 });
 const port = 8000;
