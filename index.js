8bce8753716a32fb9293f77f8509c8c6102efeb6
morukamu <makotoao8961@gmail.com>
2020-04-04 21:59:10 +0900

    ブラウザにhtmlを表示

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html><!DOCTYPE html><html lang="ja"><body><h2>HTMLの二番目に大きい見出しを表示します</h2></body></html><!DOCTYPE html><html lang="ja"><body><h3>HTMLの三番目に大きい見出しを表示します</h3></body></html><!DOCTYPE html><html lang="ja"><body><h4>HTMLの四番目に大きい見出しを表示します</h4></body></html><!DOCTYPE html><html lang="ja"><body><h5>HTMLの五番目に大きい見出しを表示します</h5></body></html>');
   res.end();
 });
 const port = 8000;
