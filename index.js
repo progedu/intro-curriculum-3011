59b247c18a2e2e3318d27153847859277dccebe5
Taku3939 <i1910040@edu.cc.uec.ac.jp>
2020-04-14 20:08:27 +0900

    update

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+  fs.readFile('./test.html', 'utf-8', (err, data) =>{
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.write(data);
+    res.end();
+  })  
+
 });
-const port = 8000;
+const port = 3000;
 server.listen(port, () => {
   console.log('Listening on ' + port);
 });
