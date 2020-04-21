5ab32732287c42a922a1e864abba112bf0e3d5b9
maiamea <maiameawork@gmail.com>
2020-04-21 19:48:54 +0900

    user-agent ではなくHTMLを表示

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    // HTML を表示させるので text/plain ではなく text/html
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  // HTML を取り出して、レスポンスの body に書き込む
+  res.write(
+    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
+);
   res.end();
 });
 const port = 8000;
