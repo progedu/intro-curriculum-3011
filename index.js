2fbaa68b67574dd41cc1f091a8c5901f403b144a
Satoshi Niikura <niikura0703@gmail.com>
2020-04-21 23:25:15 +0900

    http server 回答

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  const data = '<!DOCTYPE html>' +
+    '<html lang="ja">' +
+    '  <body>' +
+    '    <h1>HTML の一番大きな見出しを表示します</h1>' +
+    '  </body>' +
+    '</html>'
+  res.write(data);
   res.end();
 });
 const port = 8000;
