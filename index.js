841f2f668ce0e5bc595ab0d66fb51d5f1fb67c07
ss152 <64944090+ss152@users.noreply.github.com>
2020-05-13 15:37:25 +0900

    http改良

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
+  //res.write(req.headers['user-agent']);
   res.end();
 });
 const port = 8000;
