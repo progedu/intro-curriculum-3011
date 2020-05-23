9424613dc92f2e2f43a3f066ec3a3c24e2c22de6
Nobuya Okamoto <nobu.okmt@gmail.com>
2020-05-23 09:09:41 +0900

    文字列をHTMLに変更

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => { 
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>見出しだよ！</h1></body></html>');
   res.end();
 });
 const port = 8000;
 server.listen(port, () => {
   console.log('Listening on ' + port);
-});
+})
