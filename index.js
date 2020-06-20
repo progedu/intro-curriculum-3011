fbedf177d3c219caeb3a6a052df7f6e10d640031
吉田稲作 <yoshidanoinasaku@gmail.com>
2020-06-20 17:59:00 +0900

    htmlファイルを読み込んでローカルホストに表示

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+const html = fs.readFileSync('./node-text.html', 'utf8');    // htmlファイルを読み込むためには fs モジュールが必要
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(html);
   res.end();
 });
+
 const port = 8000;
 server.listen(port, () => {
   console.log('Listening on ' + port);
