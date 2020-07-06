6f8be4dac4c872a210e46d022e15288ddb678c2f
nooneknows2020 <e_dends@yahoo.co.jp>
2020-07-06 17:41:38 +0900

    HTMLファイルを読み込むように変更

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+
+const htmlData = fs.readFileSync('./index.html');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
+  res.write(htmlData);  //HTMLファイルを読み込む
   res.end();
 });
 const port = 8000;
