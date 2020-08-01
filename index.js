a463a2c26abfae0ec6d6ded08f06c97ac99d7140
taka <takahashi.toshinari.dev00@gmail.com>
2020-08-01 22:15:24 +0900

    :sparkles: ユーザエージェントではなく、htmlを返却するようにする

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 'use strict';
 const http = require('http');
+const resContent = new Array();
+
+// レスポンス内容
+resContent.push('<!DOCTYPE html>\n');
+resContent.push('<html lang="ja">\n');
+resContent.push('    <body>\n');
+resContent.push('        <h1>HTMLの一番大きい見出しを表示します</h1>\n');
+resContent.push('    </body>\n');
+resContent.push('</html>');
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(resContent.toString());
   res.end();
 });
 const port = 8000;
