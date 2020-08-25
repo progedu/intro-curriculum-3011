2584dc4942817caf038ba7caba4292901c1cf57e
Ayaha-Japan <ayaha.morinaga@gmail.com>
2020-08-25 17:12:32 +0900

    htmlテキストを追加

--- index.js
+++ index.js
@@ -2,9 +2,21 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(`<!DOCTYPE html>
+  <html lang="ja">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
+      <h1>好きな動画</h1>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/1at7kKzBYxI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h2>全く話は変わるが、バッククオテーションは改行にも対応</h2>
+  </body>
+  </html>`);
   res.end();
 });
 const port = 8000;
