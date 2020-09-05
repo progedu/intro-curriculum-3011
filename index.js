87fe090e98c136c34caedf45ce77b6a99552c104
muraken61 <murakenP61@gmail.com>
2020-09-06 06:34:04 +0900

    HTMLをレスポンスとして返すように実装

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-'use strict';
-const http = require('http');
+"use strict";
+const http = require("http");
+const fs = require("fs");
+
+const htmlData = fs.readFileSync("./index.html");
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    "Content-Type": "text/html; charset=utf-8",
   });
-  res.write(req.headers['user-agent']);
+  res.write(htmlData);
   res.end();
 });
 const port = 8000;
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+  console.log("Listening on " + port);
 });
