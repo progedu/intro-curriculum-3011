2109b20ab065f18b3794159475d3b6e70e9b5b2f
nyeoro-bong <ogasawara4039@gmail.com>
2020-09-06 16:37:31 +0900

    サーバーを立ててレスポンスにindex.htmlを実装すますた！

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+const htmlData = fs.readFileSync('./index.html');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(htmlData);
   res.end();
 });
 const port = 8000;
