777a7534753ef58d050a6edaffb2f89123dc7822
tapiokapi <68452376+tapiokapi@users.noreply.github.com>
2020-09-12 08:39:01 +0900

    html表示へ変更
    レスポンスヘッダのコンテントタイプをhtmlに変更

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 'use strict';
+// Node.jsにおけるHTTPモジュールを読み込む
 const http = require('http');
+/* サーバーを作成、関数はサーバーにリクエストがあった際に呼び出される。
+   リクエスト:req
+   レスポンス:res
+*/
 const server = http.createServer((req, res) => {
+  // 200という成功ステータスとともに、レスポンスヘッダを書き込む
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  /* resオブジェクトのwrite関数はHTTPレスポンスの内容を書き出す。
+     リクエストヘッダのuser-agentの中身を、レスポンスの内容として書き出す
+  */
+  // res.write(req.headers['user-agent']);
+
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
+
+  // endメソッドを呼び出してレスポンスの書き出しを終了する
   res.end();
 });
+// HTTPが起動するポートを宣言
 const port = 8000;
+// listen関数でサーバーを起動、特定のポートからリクエストがないかをずっと聞き耳を立てる
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+  console.log('Listening on' + port);
 });
