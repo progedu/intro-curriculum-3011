0e667b445f195ca1b44e3bbc56f754c133d5cb83
ck-n2020 <ckckck_009@yahoo.co.jp>
2020-10-10 18:27:54 +0900

    htmlファイル読み込みのencodingにutf8を指定追加

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const http = require('http');
 const fs = require('fs');
-const html = fs.readFileSync('res.html');
+const html = fs.readFileSync('res.html', 'utf8');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
