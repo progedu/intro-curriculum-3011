781d012458512cc45fbb8538e979eabf8415a45d
ck-n2020 <ckckck_009@yahoo.co.jp>
2020-10-10 18:15:05 +0900

    htmlを表示するように改修

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+const html = fs.readFileSync('res.html');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(html);
   res.end();
 });
 const port = 8000;
