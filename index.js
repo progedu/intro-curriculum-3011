76d2d6752178b74943e061c8adc32403bc740ccb
heavyfox <steamtrader@gmai.com>
2020-10-13 19:49:47 +0900

    htmlの表示

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
+  res.write(
+    '<!DOCTYPE html><html lang="ja"><body><h3>あなたのユーザーエージェントを表示しています</h3></body></html>'
+  );
+
+
   res.write(req.headers['user-agent']);
   res.end();
 });
