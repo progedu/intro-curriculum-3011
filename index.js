1dd14be25c125da25a6c4d9d7d354918db6db903
neutral-kaori <kasiwamoti5@gmail.com>
2020-10-13 20:08:25 +0900

    "授業を聞いた後、修正しました。"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
     'Content-Type': 'text/html; charset=utf-8'
   });
   res.write(
-    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
+    '<!DOCTYPE html><html lang="ja"><body><p>user-agentのところを変えるんだろうとは思ったんだけど、どういれたら良いのか分からなかった。</p></body></html>'
     );
   res.end();
 });
