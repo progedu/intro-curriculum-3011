906c0b0695c998dafe96fbc058364c3a9ee38ef4
nanachan2020 <dh_shibata@yahoo.co.jp>
2020-10-13 20:36:15 +0900

    HTMLの内容を表示させる

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+
+  const doc = 
+  `<!DOCTYPE html>
+  <html lang="ja">
+    <body>
+      <h1>HTMLの一番大きい見出しを表示します</h1>
+    </body>
+  </html>`
+  res.write(doc);
+
+  // res.write('<!DOCTYPE html>\n<html lang="ja">\n<body>\n<h1>HTMLの一番大きい見出しを表示します</h1>\n</body>\n</html>');
   res.end();
 });
 const port = 8000;
