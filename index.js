643e3d00f4447766f9981ddfc85b74fe3597839d
rascasse-monaco <mktwkhr@gmail.com>
2020-10-13 20:00:27 +0900

    練習問題の回答

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');//fsモジュール呼び出し。
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+  //ファイル読み込み。
+  fs.readFile('./html/index.html', 'UTF-8',  (err, data) => {
+    //エラーのログを出す。
+    if (err) {
+      console.log('ファイルを読み込むことができませんでした。');
+    }
+    //書き出し。
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write(data);
+    res.end();
   });
-  res.write(req.headers['user-agent']);
-  res.end();
 });
+
 const port = 8000;
 server.listen(port, () => {
   console.log('Listening on ' + port);
