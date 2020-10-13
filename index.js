e4079082fd9461b40b79ac527c062e2f217c6db4
mtmtP <matsumoto0916@gmail.com>
2020-10-13 20:01:53 +0900

    htmlを出力

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 'use strict';
+const html = `<!DOCTYPE html>
+<html lang="ja">
+  <body>
+    <h1>今日のおつまみ候補</h1>
+    <ul>
+    <li>サンマ</li>
+    <li>鶏レバー</li>
+    <li>ヤゲン軟骨</li>
+    </ul>
+  </body>
+</html>`;
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(html);
   res.end();
 });
 const port = 8000;
