4ee9638d50560851360ab9451d355021a8ce24d8
CoreiNion <supermario256w@icloud.com>
2020-10-13 20:11:28 +0900

    h1とおまけをを表示

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 const http = require('http');
+const { uname } = require('node-uname')
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html> <html lang="ja"> <body> <h1> Kernel:' + uname().release + '</h1> <br> <h1> HTMLの一番大きい見出しを表示します</h1> </body> </html>');
   res.end();
 });
 const port = 8000;
