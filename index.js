4ec07a87056b205327aa09d922d67f3c8ef08c9b
blue-one1975 <hiroshitakeda428@yahoo.co.jp>
2020-10-13 20:04:05 +0900

    html,cssファイルも送れるように実装

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 'use strict';
+// const http = require('http');
+// const fs = require('fs');
+// const data = fs.readFileSync('./a.html');
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/html; charset=utf-8'
+//   });
+//   res.write(data);
+//   res.end();
+// });
+// const port = 8000;
+// server.listen(port, () => {
+//   console.log(`Listening On ${port}`);
+// });
+
+
+// const http = require('http');
+// const fs = require('fs');
+
+// fs.readFile('./a.html', (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+//     res.write(data);
+//     res.end();
+//   }).listen(8000);
+//   console.log(`Server Running...`);
+// });
+
+
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+  if(req.url === '/') {
+    fs.readFile('./b.html', 'utf-8', (err, html) => {
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      res.write(html);
+      res.end();
+    });
+  } else if (req.url.match('\.css$')) {
+    const rs = fs.createReadStream('./style.css', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/css; charset=utf-8' });
+    rs.pipe(res);
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8' });
+    res.end('No Page Found');
+  }
 });
+
 const port = 8000;
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+  console.log(`Sever Running...${port}`);
 });
