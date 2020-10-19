7622348727eb804d8e28829f6e9d4427a6cf6148
Aozora Okada <bluesky125osmkk@gmail.com>
2020-10-19 19:30:17 +0900

    h2タグと色を追加

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
     'Content-Type': 'text/html; charset=utf-8'
   });
   res.write(
-    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
+    '<!DOCTYPE html><html lang="ja"><body><h1><font color="orangered">HTMLの一番大きい見出しを表示します</font></h1><h2><font color="deepskyblue">HTMLの二番目に大きい見出しを表示します</font></h2></body></html>'
   );
   res.end();
 });
