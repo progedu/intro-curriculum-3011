114436dd1b5a2f2acdcfeba7ce7af987baf24ab5
Yasuomi Harada <yasuomi-8303@mineo.jp>
2020-11-17 23:32:45 +0900

    HTMLタグを表示させるに変更

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1><br><h2>二番目に大きい見出しも追加</h2></body></html>');
   res.end();
 });
 const port = 8000;
