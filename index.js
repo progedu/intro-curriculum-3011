378f83dd51edb60f3947108973c95c63727a83e2
Kazuho Maejima <kazuho_maejima@dwango.co.jp>
2020-11-23 10:02:33 +0900

    練習問題 HTMLを表示するように変更

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+  fs.readFile('./practice.html', 'utf-8', doRead);
+  function doRead(err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.write(data);
+    res.end();
+  }
 });
 const port = 8000;
 server.listen(port, () => {
