cf201a5c590a7ecf2ab5ce199cf70d6ecfed6429
adoptyano <adoptyano@gmail.com>
2020-12-13 16:38:56 +0900

    練習問題

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 const http = require('http');
+let hoge = '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+    res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.write(hoge);
+    res.end();
 });
 const port = 8000;
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+    console.log('Listenin on' + port);
 });
