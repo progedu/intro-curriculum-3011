dd0d30e91cc46836b0507add38b99fb66532f074
Lisa Hirano <risa_hirano@dwango.co.jp>
2021-01-05 11:56:22 +0900

    練習問題の実装

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
@@ -7,23 +6,6 @@ const server = http.createServer((req, res) => {
   });
   res.write(
     '<!DOCTYPE html><html lang = "ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
-  );
-  res.end();
-});
-const port = 8000;
-server.listen(port, () => {
-  console.log('Listening on ' + port);
-});
-*/
-
-'use strict';
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
-  });
-  res.write(
-    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
   );
   res.end();
 });
