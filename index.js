93e9afc71ceacd6e23653eb5b6bc8d3178e568a5
aozametatuki <akomatsu1224@gmail.com>
2021-01-24 21:07:25 +0900

    レスポンス内容の修正

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 'use strict';
+
+// httpのモジュールを読み込む
 const http = require('http');
+
+// サーバーを作成
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+    // レスポンスヘッダ
+    res.writeHead(200, {
+        'Content-Type' : 'text/html; charset=utf-8'
+    });
+    // レスポンスの内容
+    res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
+    // レスポンスの書き出し終了
+    res.end();
 });
+
+// httpが起動するポートを設定
 const port = 8000;
+// サーバーを起動
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+    console.log('Listening on ' + port);
 });
