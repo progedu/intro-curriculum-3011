57d65a59703e2a27818f46433c3407653ecf54f6
Kengo Yagishita <yagishitakengo1002@gmail.com>
2021-05-12 22:44:37 +0900

    レスポンスにHTML表示

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  // レスポンスヘッダのコンテンツタイプを 'text/html' に変更
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  // レスポンスにHTMLを書き込み
+  res.write(
+    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
+  );
   res.end();
 });
 const port = 8000;
