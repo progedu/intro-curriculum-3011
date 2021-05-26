9337c300df0d19cfe1c1412d28076a19055536d7
shiraki <srkkyoko@outlook.jp>
2021-05-26 17:48:23 +0900

    コミット時のコメント

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
-  res.end();
+  //res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html>');
+  res.write('<html lang="ja">');
+  res.write('<body>');
+  res.write('<h1>HTMLの一番大きい見出しを表示します</h1>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();  
 });
 const port = 8000;
 server.listen(port, () => {
