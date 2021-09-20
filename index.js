b453bf80c462812bd95b649ad8f7a714ea837bd2
Kento Futatsugami <futatsugami_kento@ideal-ltd.jp>
2021-09-21 00:29:58 +0900

    実装完了

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+const fs = require('fs');
+
+const server = http.createServer(
+  (req, res) => {
+    fs.readFile('./test.html', 'UTF-8',(error, data) => {
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+      });
+      res.write(data);
+      res.end();
+    });
   });
-  res.write(req.headers['user-agent']);
-  res.end();
-});
 const port = 8000;
 server.listen(port, () => {
   console.log('Listening on ' + port);
